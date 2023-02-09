4d2e8b460f84a2b5a0ac491ed0e31b9dc81795c8
alimbaikyzyadelya <adelaalieva33@gmail.com>
2023-02-09 22:31:10 +0000

    projeck

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Goots from './pages/goots/Goots';
+import Catalog from './pages/Catalog/Catalog';
+import './scss/style.scss'
+
+function App() {
+  return (
+    <>
+      {/* <Catalog/> */}
+      <Goots/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/goots/Goots.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import elean from '../../Assets/goots/EleanBoutique20983 2 (1).png'
+import lag from '../../Assets/goots/elean47408 3.png'
+import elen from '../../Assets/goots/elean1_58886 2 (1).png'
+import img from '../../Assets/goots/Elean25-sajt2 1 (1).png'
+import image from '../../Assets/goots/image1.png'
+import img1 from '../../Assets/goots/img2.png'
+
+
+
+const Goots = () => {
+  return (
+    
+    <div className='conteiner'>
+      <div className='goots'>
+        <div className='goots__h1'>ВЫГЛЯДЕТЬ БЛИСТАТЕЛЬНО ПОМОЖЕТ</div>
+        <div className='goots__blok'>
+          <div className="goots__line">
+
+          </div>
+          <div className='elean__1'>
+        <img className='goots__img' src={elean} alt="" />
+        <h2 className='goots__tuxedo'>/ Платье-смокинг длинное</h2>
+        <p className='goots__heart'><svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11.5703 3.70142C10.9351 3.10858 8.50169 0.978395 6.34687 1.00017C4.13844 1.02277 0.729548 2.52245 1.01707 6.9662C1.2713 10.8916 11.4013 18.9636 11.4013 18.9636C11.4647 19.0121 11.5305 19.0121 11.5987 18.9636C11.5987 18.9636 21.7287 10.8908 21.9829 6.9662C22.2705 2.52245 18.8616 1.02277 16.6523 1.00017C14.1499 0.975046 11.2714 3.85214 11.2714 3.85214L9.31716 5.91953" stroke="#222222" stroke-miterlimit="3.8637"/>
+            </svg>
+        </p>
+
+        <p className='goots__price'>24 900 ₽</p>
+        
+      <p className='goots__text'>Подчеркнет линию талии</p>
+        </div>
+        <div className='elean__1'>
+        <img className='goots__img' src={lag} alt=""/>
+        <h2 className='goots__tuxedo'>/ Белый смокинг и брюки с лампасами</h2>
+        <p className='goots__heart'><svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.5703 3.70142C10.9351 3.10858 8.50169 0.978395 6.34687 1.00017C4.13844 1.02277 0.729548 2.52245 1.01707 6.9662C1.2713 10.8916 11.4013 18.9636 11.4013 18.9636C11.4647 19.0121 11.5305 19.0121 11.5987 18.9636C11.5987 18.9636 21.7287 10.8908 21.9829 6.9662C22.2705 2.52245 18.8616 1.02277 16.6523 1.00017C14.1499 0.975046 11.2714 3.85214 11.2714 3.85214L9.31716 5.91953" stroke="#222222" stroke-miterlimit="3.8637"/>
+</svg>
+</p>
+        <p className='goots__price'>41 400 ₽</p>
+        
+<p className='goots__text'>Подчеркнет линию талии</p>
+        </div>
+        <div className='elen__1'>
+        <img className='goots__img' src={elen} alt=""/>
+        <h2 className='goots__tuxedo'>/ Классический комплект с бабочкой</h2>
+
+        <p className='goots__heart'><svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.5703 3.70142C10.9351 3.10858 8.50169 0.978395 6.34687 1.00017C4.13844 1.02277 0.729548 2.52245 1.01707 6.9662C1.2713 10.8916 11.4013 18.9636 11.4013 18.9636C11.4647 19.0121 11.5305 19.0121 11.5987 18.9636C11.5987 18.9636 21.7287 10.8908 21.9829 6.9662C22.2705 2.52245 18.8616 1.02277 16.6523 1.00017C14.1499 0.975046 11.2714 3.85214 11.2714 3.85214L9.31716 5.91953" stroke="#222222" stroke-miterlimit="3.8637"/>
+</svg>
+</p>
+        <p className='goots__price'>51 800 ₽</p>
+        
+<p className='goots__text'>Подчеркнет линию талии</p>
+        </div>
+        <div className='elean_1'>
+        <img className='goots__img' src={img} alt=""/>
+        <h2 className='goots__tuxedo'>/ Блуза с баской</h2>
+
+        <p className='goots__heart'><svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11.5703 3.70142C10.9351 3.10858 8.50169 0.978395 6.34687 1.00017C4.13844 1.02277 0.729548 2.52245 1.01707 6.9662C1.2713 10.8916 11.4013 18.9636 11.4013 18.9636C11.4647 19.0121 11.5305 19.0121 11.5987 18.9636C11.5987 18.9636 21.7287 10.8908 21.9829 6.9662C22.2705 2.52245 18.8616 1.02277 16.6523 1.00017C14.1499 0.975046 11.2714 3.85214 11.2714 3.85214L9.31716 5.91953" stroke="#222222" stroke-miterlimit="3.8637"/>
+                  </svg>
+        </p>
+
+        <p className='goots__price'>12 900 ₽</p>
+        
+        <p className='goots__text'>Подчеркнет линию талии</p>
+        </div>
+        </div>
+        
+      </div> 
+
+
+      <div className="goots__twoblock">
+        <div className="goots__twoblock-parralelogram">
+        <div className="goots__twoblock-up">
+          <img className='' src={image} alt="" />
+        </div>
+        <div className="goots__twoblock-lower">
+          <img src={img1} alt="" />
+        </div>
+        </div>
+        
+        
+        
+        
+      </div>
+    </div>
+    
+    
+
+  )
+}
+
+export default Goots
